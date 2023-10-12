6f43dfad092b1d34cff8cd82679ec1353b747611
prakash855 <sshivamprakashh301@gmail.com>
2023-10-12 15:40:54 +0530

    socket frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,123 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+import { Card, Avatar, Input, Typography } from "antd";
+import "./App.css";
 
-function App() {
+const { Search } = Input;
+const { Text } = Typography;
+const { Meta } = Card;
+
+const client = new W3CWebSocket("ws://127.0.0.1:8000");
+
+const App = () => {
+  const [userName, setUserName] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [searchVal, setSearchVal] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const onButtonClicked = (value) => {
+    client.send(
+      JSON.stringify({
+        type: "message",
+        msg: value,
+        user: userName,
+      })
+    );
+    setSearchVal("");
+  };
+
+  useEffect(() => {
+    client.onopen = () => {
+      console.log("WebSocket Client Connected");
+    };
+
+    client.onmessage = (message) => {
+      const dataFromServer = JSON.parse(message.data);
+      console.log("got reply! ", dataFromServer);
+      if (dataFromServer.type === "message") {
+        setMessages((prevMessages) => [
+          ...prevMessages,
+          {
+            msg: dataFromServer.msg,
+            user: dataFromServer.user,
+          },
+        ]);
+      }
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main" id="wrapper">
+      {isLoggedIn ? (
+        <div>
+          <div className="title">
+            <Text
+              id="main-heading"
+              type="secondary"
+              style={{ fontSize: "36px" }}
+            >
+              Websocket Chat: {userName}
+            </Text>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              paddingBottom: 50,
+            }}
+            id="messages"
+          >
+            {messages.map((message) => (
+              <Card
+                key={message.msg}
+                style={{
+                  width: 300,
+                  margin: "16px 4px 0 4px",
+                  alignSelf:
+                    userName === message.user ? "flex-end" : "flex-start",
+                }}
+                loading={false}
+              >
+                <Meta
+                  avatar={
+                    <Avatar
+                      style={{ color: "#f56a00", backgroundColor: "#fde3cf" }}
+                    >
+                      {message.user[0].toUpperCase()}
+                    </Avatar>
+                  }
+                  title={message.user + ":"}
+                  description={message.msg}
+                />
+              </Card>
+            ))}
+          </div>
+          <div className="bottom">
+            <Search
+              placeholder="input message and send"
+              enterButton="Send"
+              value={searchVal}
+              size="large"
+              onChange={(e) => setSearchVal(e.target.value)}
+              onSearch={(value) => onButtonClicked(value)}
+            />
+          </div>
+        </div>
+      ) : (
+        <div style={{ padding: "200px 40px" }}>
+          <Search
+            placeholder="Enter Username"
+            enterButton="Login"
+            size="large"
+            onSearch={(value) => {
+              setIsLoggedIn(true);
+              setUserName(value);
+            }}
+          />
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
